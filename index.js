2d0cace5f53976f17b001212ef086d5c02c3f589
Samantha brake <sammy.brake@gmail.com>
2019-05-18 15:37:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function BoardMember(name, homeState, training) {
   this.name = name;
-  this.homeState = homeState; 
-  this.training = training; 
+  this.homeState = homeState;
+  this.training = training;
 };
