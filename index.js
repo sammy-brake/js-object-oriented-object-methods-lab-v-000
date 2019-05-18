1eb217e1623bc366f2f22af4bfbdf1d3e9d18df4
Samantha brake <sammy.brake@gmail.com>
2019-05-18 15:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ function BoardMember(name, homeState, training) {
   this.veto = function() {
     return "No, I must disagree";
   };
+
+  this.approve = function() {
+    return "You can do that!";
+  };
+
+  this.doCharity = function() {
+    return "I like to help people.";
+  };
 };
-// approve — returns You can do that!
 // doCharity — returns I like to help people.
 // releasePressStatement — returns You will see great things from Scuber.
 // sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
