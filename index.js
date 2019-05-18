8f51f47e9070b4f49b1ef379ac1c6be35362119c
Samantha brake <sammy.brake@gmail.com>
2019-05-18 15:41:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ function BoardMember(name, homeState, training) {
   this.releasePressStatement = function() {
     return "You will see great things from Scuber."
   }
+
+  this.sayHi = function() {
+    return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.``
+
+  }
 };
 
-// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
