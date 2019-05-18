7cfdd3d2f2f0936ed99cc7afca0e980574e84e62
Samantha brake <sammy.brake@gmail.com>
2019-05-18 15:38:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,7 @@ function BoardMember(name, homeState, training) {
     return "No, I must disagree";
   };
 };
-veto — returns No, I must disagree
-
-approve — returns You can do that!
-
-doCharity — returns I like to help people.
-
-releasePressStatement — returns You will see great things from Scuber.
-
-sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+// approve — returns You can do that!
+// doCharity — returns I like to help people.
+// releasePressStatement — returns You will see great things from Scuber.
+// sayHi — returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
