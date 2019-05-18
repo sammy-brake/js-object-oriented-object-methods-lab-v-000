83cf6eef15818154389bf051e3c83ce442653c1f
Samantha brake <sammy.brake@gmail.com>
2019-05-18 15:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,5 @@ function BoardMember(name, homeState, training) {
 
   this.sayHi = function() {
     return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}.`
-
   }
 };
